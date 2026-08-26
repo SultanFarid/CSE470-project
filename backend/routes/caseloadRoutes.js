39d6d1342384c1e727f7d976e921f0902f966f66
SultanFarid <moahammadfaridsultan@gmail.com>
2026-08-26 16:47:21 +0600

    A & E

--- backend/routes/caseloadRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { verifyToken, isTherapist } = require('../middleware/authMiddleware');
-const controller = require('../controllers/caseloadController');
-
-router.get('/my', verifyToken, isTherapist, controller.getMyCaseload);
-
-module.exports = router;
